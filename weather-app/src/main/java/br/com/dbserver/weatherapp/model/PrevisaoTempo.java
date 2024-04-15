d5c8f4beff5098422f80c11678148bbb89a1ea3e
pedro felipe <pedrofelipinho@gmail.com>
2024-04-15 11:36:51 -0300

    Adicionando o model PrevisãoTempo

---
+++ weather-app/src/main/java/br/com/dbserver/weatherapp/model/PrevisaoTempo.java
@@ -0,0 +1,23 @@
+package br.com.dbserver.weatherapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PrevisaoTempo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cidade;
+    private String tempo;
+}
