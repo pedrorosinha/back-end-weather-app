f4e5e267a0ca08f7feb40094abc38f9fad5e47aa
pedro felipe <pedrofelipinho@gmail.com>
2024-04-15 15:46:07 -0300

    Excluindo 2 imports na controller

--- weather-app/src/main/java/br/com/dbserver/weatherapp/controllers/PrevisaoTempoController.java
+++ weather-app/src/main/java/br/com/dbserver/weatherapp/controllers/PrevisaoTempoController.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
