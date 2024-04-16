005c07ce487f44b62943c7f49069fd9cfb0f014a
pedro felipe <pedrofelipinho@gmail.com>
2024-04-16 09:35:43 -0300

    Adicionando o @Repository

--- weather-app/src/main/java/br/com/dbserver/weatherapp/repository/PrevisaoTempoRepository.java
+++ weather-app/src/main/java/br/com/dbserver/weatherapp/repository/PrevisaoTempoRepository.java
@@ -2,7 +2,9 @@ package br.com.dbserver.weatherapp.repository;
 
 import br.com.dbserver.weatherapp.model.PrevisaoTempo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PrevisaoTempoRepository extends JpaRepository<PrevisaoTempo, Long> {
 
 }
