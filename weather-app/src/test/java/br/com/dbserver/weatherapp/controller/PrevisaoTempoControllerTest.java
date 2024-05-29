b6545224d9bc03fc44f1cb68d751eb3cb8a5069e
pedro felipe <pedrofelipinho@gmail.com>
2024-05-29 10:46:47 -0300

    Refatorando o teste do controller

--- weather-app/src/test/java/br/com/dbserver/weatherapp/controller/PrevisaoTempoControllerTest.java
+++ weather-app/src/test/java/br/com/dbserver/weatherapp/controller/PrevisaoTempoControllerTest.java
@@ -3,8 +3,9 @@ package br.com.dbserver.weatherapp.controller;
 import br.com.dbserver.weatherapp.WeatherAppApplication;
 import br.com.dbserver.weatherapp.controllers.PrevisaoTempoController;
 import br.com.dbserver.weatherapp.dto.PrevisaoDTO;
+import br.com.dbserver.weatherapp.enums.Clima;
+import br.com.dbserver.weatherapp.enums.Turno;
 import br.com.dbserver.weatherapp.services.interf.PrevisaoTempoService;
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,7 +24,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = WeatherAppApplication.class)
-class PrevisaoTempoControSllerTest {
+class PrevisaoTempoControllerTest {
 
     @Mock
     private PrevisaoTempoService previsaoTempoService;
@@ -38,54 +40,36 @@ class PrevisaoTempoControSllerTest {
     }
 
     @Test
-    void testObterPrevisaoHojeSucesso() {
-        PrevisaoDTO previsaoDTO = new PrevisaoDTO(cidade, "Ensolarado");
+    void testObterPrevisaoAtualSucesso() {
+        PrevisaoDTO previsaoDTO = new PrevisaoDTO(cidade, Turno.MANHA, Clima.ENSOLARADO, 25, 32, 5, 60, 10, LocalDate.now());
 
         when(previsaoTempoService.obterPrevisaoTempoAtual(cidade)).thenReturn(previsaoDTO);
 
-        ResponseEntity<PrevisaoDTO> response = previsaoTempoController.obterPrevisaoHoje(cidade);
+        ResponseEntity<PrevisaoDTO> response = previsaoTempoController.obterPrevisaoAtual(cidade);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(previsaoDTO, response.getBody());
     }
 
     @Test
-    void testObterPrevisaoHojeErro() {
-        when(previsaoTempoService.obterPrevisaoTempoAtual(cidade)).thenThrow(new EntityNotFoundException());
-
-        ResponseEntity<PrevisaoDTO> response = previsaoTempoController.obterPrevisaoHoje(cidade);
-
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-    }
-
-    @Test
-    void testObterPrevisaoHojeErroServidor() {
-        when(previsaoTempoService.obterPrevisaoTempoAtual(cidade)).thenThrow(new RuntimeException());
-
-        ResponseEntity<PrevisaoDTO> response = previsaoTempoController.obterPrevisaoHoje(cidade);
-
-        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-    }
-
-    @Test
-    void testObterPrevisaoSemana() {
-        List<PrevisaoDTO> previsoesDTO = Arrays.asList(new PrevisaoDTO(cidade, "Chuvoso"), new PrevisaoDTO(cidade, "Nublado"));
+    void testObterPrevisaoProximos7Dias() {
+        List<PrevisaoDTO> previsoesDTO = Arrays.asList(new PrevisaoDTO(cidade, Turno.MANHA, Clima.CHUVOSO, 20, 25, 10, 80, 15, LocalDate.now().plusDays(1)), new PrevisaoDTO(cidade, Turno.TARDE, Clima.NUBLADO, 22, 28, 5, 70, 10, LocalDate.now().plusDays(2)));
 
         when(previsaoTempoService.obterPrevisaoProximos7Dias(cidade)).thenReturn(previsoesDTO);
 
-        ResponseEntity<List<PrevisaoDTO>> response = previsaoTempoController.obterPrevisaoSemana(cidade);
+        ResponseEntity<List<PrevisaoDTO>> response = previsaoTempoController.obterPrevisaoProximos7Dias(cidade);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(previsoesDTO, response.getBody());
     }
 
     @Test
-    void testGetAllPrevisoes() {
-        List<PrevisaoDTO> previsoesDTO = Arrays.asList(new PrevisaoDTO("São Paulo", "Ensolarado"), new PrevisaoDTO("Porto Alegre", "Chuvoso"));
+    void testObterTodasPrevisoes() {
+        List<PrevisaoDTO> previsoesDTO = Arrays.asList(new PrevisaoDTO("São Paulo", Turno.MANHA, Clima.ENSOLARADO, 25, 32, 5, 60, 10, LocalDate.now()), new PrevisaoDTO("Porto Alegre", Turno.TARDE, Clima.CHUVOSO, 22, 28, 10, 85, 12, LocalDate.now()));
 
-        when(previsaoTempoService.getAllPrevisoesAsDTO()).thenReturn(previsoesDTO);
+        when(previsaoTempoService.getAllPrevisoes()).thenReturn(previsoesDTO);
 
-        ResponseEntity<List<PrevisaoDTO>> response = previsaoTempoController.getAllPrevisoes();
+        ResponseEntity<List<PrevisaoDTO>> response = previsaoTempoController.obterTodasPrevisoes();
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(previsoesDTO, response.getBody());
@@ -93,7 +77,7 @@ class PrevisaoTempoControSllerTest {
 
     @Test
     void testCadastrarPrevisaoSucesso() {
-        PrevisaoDTO previsaoDTO = new PrevisaoDTO(cidade, "Chuvoso");
+        PrevisaoDTO previsaoDTO = new PrevisaoDTO(cidade, Turno.MANHA, Clima.CHUVOSO, 20, 28, 10, 70, 15, LocalDate.now());
 
         when(previsaoTempoService.cadastrarPrevisao(any(PrevisaoDTO.class))).thenReturn(previsaoDTO);
 
@@ -106,7 +90,7 @@ class PrevisaoTempoControSllerTest {
     @Test
     void testAtualizarPrevisaoSucesso() {
         Long id = 1L;
-        PrevisaoDTO previsaoDTO = new PrevisaoDTO(cidade, "Nublado");
+        PrevisaoDTO previsaoDTO = new PrevisaoDTO(cidade, Turno.TARDE, Clima.NUBLADO, 22, 30, 5, 65, 12, LocalDate.now());
 
         when(previsaoTempoService.atualizarPrevisao(eq(id), any(PrevisaoDTO.class))).thenReturn(previsaoDTO);
 
@@ -117,14 +101,11 @@ class PrevisaoTempoControSllerTest {
     }
 
     @Test
-    void testAtualizarPrevisaoErro() {
+    void testDeletarPrevisaoSucesso() {
         Long id = 1L;
-        PrevisaoDTO previsaoDTO = new PrevisaoDTO(cidade, "Nublado");
 
-        when(previsaoTempoService.atualizarPrevisao(eq(id), any(PrevisaoDTO.class))).thenThrow(new EntityNotFoundException());
+        ResponseEntity<Void> response = previsaoTempoController.deletarPrevisao(id);
 
-        ResponseEntity<PrevisaoDTO> response = previsaoTempoController.atualizarPrevisao(id, previsaoDTO);
-
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
 }
